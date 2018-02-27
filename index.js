50c611ab7ed43f671ee521969e8628c22420bc1f
N Eliseo S Carranza <neli92santscz@gmail.com>
2018-02-26 22:25:11 -0500

    new

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import Media from "./src/playlist/components/media"
 
 const app = document.getElementById("app");
 
-ReactDOM.render(<Media/>,app);
+ReactDOM.render(<Media title="Hola mundo" author="NehemiasEC" image="./images/328.jpg"/>,app);
 
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,21 +1,43 @@
 import React from "react";
-import "./media.css"
-
+import "./media.css";
+import PropTypes from "prop-types";
 
 class Media extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            author:props.author
+        }
+        //this.handleClick= this.handleClick.bind(this)
+    }
+
+    handleClick=(event)=>{
+        console.log(this.props.title);
+        this.setState({
+            author:"EliseoSC"
+        });
+    }
+
     render(){
         return(
-            <div className="Media">
+            <div className="Media" onClick={this.handleClick}>
                 <div>
-                    <img src="./images/328.jpg" alt="" width={260} height={160}/>
-                    <h3>React</h3>
-                    <p>NehemiasEC</p>
+                    <img src={this.props.image} alt="" width={260} height={160}/>
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p>{this.state.author}</p>
                 </div>
             </div>
         )
     }
 }
 
+
+Media.propTypes={
+    image:PropTypes.string,
+    title:PropTypes.string,
+    author:PropTypes.string
+
+};
 
 export default Media;
 
